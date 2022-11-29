2f851844d7b554ed31863247513237267d773c51
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-11-29 21:42:18 +0200

    Assign event handlers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import 'normalize.css';
 import './sass/style.scss';
-import { onSidebarIconClick } from './modules/util.mjs';
+import * as eventHandlers from './modules/event_handlers.mjs';
 
-const menuIconElem = document.querySelector('header .menu-icon');
+// TODO: generate default projects from storage (on current default tab)
 
-menuIconElem.onclick = onSidebarIconClick;
+const modalElem = document.querySelector('.modal');
+const headerElem = document.querySelector('header');
+const sidebarElem = document.querySelector('.sidebar');
+const headerPanelElem = document.querySelector('.header-panel');
+const todoContainerElem = document.querySelector('.todo-container');
+
+modalElem.addEventListener('click', eventHandlers.modalHandler);
+headerElem.addEventListener('click', eventHandlers.headerHandler);
+sidebarElem.addEventListener('click', eventHandlers.sidebarHandler);
+headerPanelElem.addEventListener('click', eventHandlers.headerPanelHandler);
+todoContainerElem.addEventListener('click', eventHandlers.todoContainerHandler);
