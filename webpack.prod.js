e9e4dd91f534052eb337988d83d694955a9a67f8
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-12-06 01:24:44 +0200

    Make minor fixes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,17 @@
 import common from './webpack.common.js';
 import { merge } from 'webpack-merge';
+import TerserPlugin from 'terser-webpack-plugin';
 
 export default merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true
+        },
+      }),
+    ],
+  },
 });
